3d147d007d92bd856f1708fc1e04055ceda6dfa3
Tsutomu Kawamura <cognitom@gmail.com>
2016-07-15 16:58:16 +0900

    Makes configBuilder apart from index.js

--- index.js
+++ index.js
@@ -5,47 +5,11 @@ const
   log = require('loglevel'),
   refresh = require('./lib/refresh'),
   watch = require('./lib/watch'),
-  handler = require('./lib/handler')
-
-/** default values */
-const defaults = {
-  src: null,
-  cache: 'cache',
-  root: process.cwd(),
-  handlers: {}, // default handlers for each extension
-  patterns: [], // array of globs to handle
-  update: 'once', // 'never', 'once' or 'allways'
-  refresh: true, // refresh on starting
-  watch: false,
-  maxAge: 0,
-  debug: false
-}
+  handler = require('./lib/handler'),
+  configBuilder = require('./lib/config-builder')
 
 module.exports = function(...configs) {
-  const opts = Object.assign({}, defaults)
-
-  // composition of recipes, configs and overwrites
-  for (const config of configs) {
-    config.handlers = Object.assign(opts.handlers, config.handlers || {})
-    Object.assign(opts, config)
-  }
-
-  // some checks and modifications
-  if (!opts.src) throw new Error('Felt needs src directory. Ex: "public"')
-  if (!opts.patterns.length) {
-    opts.patterns = Object.keys(opts.handlers).map(ext => `**/*${ ext }`)
-  }
-
-  // wires up the patterns and handlers
-  opts.patterns = opts.patterns.map(pattern => {
-    if (typeof pattern == 'string') pattern = { pattern }
-    if (!pattern.pattern) throw new Error('No pattern')
-    if (pattern.handler) return pattern
-    for (const ext in opts.handlers)
-      if (new RegExp(`\\${ ext }$`).test(pattern.pattern))
-        return Object.assign(pattern, { handler: opts.handlers[ext] })
-    throw new Error(`No handler refistered: ${ pattern }`)
-  })
+  const opts = configBuilder(...configs)
 
   const loglevel = opts.debug ? log.levels.DEBUG : log.levels.ERROR
   log.setLevel(loglevel, false)
